b5f2263b1a453977dd2e135533103eac757325a5
Sebastian Roldan Daza <roldansebasdaza@gmail.com>
2025-04-06 12:24:08 -0500

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
     starlight({
       title: 'THE BARBER HOUSE',
       logo: {src: './src/assets/3.png',},
-      favicon: '/favicon.ico',
+      favicon: './favicon.ico',
       // plugins: [
       //   ion()
       // ],
